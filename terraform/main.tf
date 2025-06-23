7dfa72e1a1721cf3b4b8c2bbd87d9405abb98506
Ramanuj Dad <rampooja245@gmail.com>
2025-06-23 13:00:54 +0530

    imporved deployment process

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,37 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "deployment_automation" {
+  metadata {
+    name = "deployment-automation"
+  }
+}
+
+resource "kubernetes_config_map" "deployment_config" {
+  metadata {
+    name      = "deployment-config"
+    namespace = kubernetes_namespace.deployment_automation.metadata[0].name
+  }
+  data = {
+    cluster_ip = var.cluster_ip
+  }
 }
 
 resource "kubernetes_namespace" "ingress" {
@@ -21,9 +47,43 @@ resource "kubernetes_namespace" "ingress" {
 resource "kubernetes_config_map" "cluster_info" {
   metadata {
     name      = "cluster-info"
-    namespace = "ingress-nginx"
+    namespace = kubernetes_namespace.ingress.metadata[0].name
   }
   data = {
     cluster_ip = var.cluster_ip
   }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = kubernetes_namespace.ingress.metadata[0].name
+  create_namespace = false
+
+  values = [
+    yamlencode({
+      controller = {
+        service = {
+          type = "NodePort"
+          nodePorts = {
+            http  = 30080
+            https = 30443
+          }
+        }
+        replicaCount = 1
+        tolerations = [
+          {
+            key      = "node-role.kubernetes.io/control-plane"
+            operator = "Equal"
+            effect   = "NoSchedule"
+          }
+        ]
+        hostNetwork = true
+        dnsPolicy   = "ClusterFirstWithHostNet"
+      }
+    })
+  ]
+
+  depends_on = [kubernetes_namespace.ingress]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,14 @@ output "cluster_ip" {
   value = var.cluster_ip
 }
 
+output "deployment_namespace" {
+  value = kubernetes_namespace.deployment_automation.metadata[0].name
+}
+
 output "ingress_namespace" {
   value = kubernetes_namespace.ingress.metadata[0].name
+}
+
+output "nginx_ingress_status" {
+  value = helm_release.nginx_ingress.status
 }
